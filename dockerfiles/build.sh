0d7018a561deee9a9fb3c147c9b9b0ede1e3a3dc
kongbuhaja <pcar530@gmail.com>
2026-01-19 20:53:28 +0900

    update docker file

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -1,3 +1,2 @@
 sudo docker build -f "${PWD}/env.dockerfile" -t tf_yolo:1.0 .
-# sudo docker run --gpus '"device=0"' --cpuset-cpus=16-23 -m 16g --shm-size=8g -it -v "${PWD%/*/*}":/home/ML --name test tf_yolo:1.0
-sudo docker run --cpuset-cpus=16-23 -m 64g --shm-size=8g -it -v "${PWD%/*/*}":/home/ML -e HOST_PERMS="$(id -u):$(id -g)" --name test tf_yolo:1.0
+sudo docker run --gpus '"device=0"' -m 64g --shm-size=8g -it -v "${PWD%/*/*}":/home/ML -e HOST_PERMS="$(id -u):$(id -g)" --name tf_yolo tf_yolo:1.0
