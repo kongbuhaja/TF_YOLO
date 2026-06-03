d81449a7f21bfce1e8c02362640675444dad0243
kongbuhaja <pcar530@gmail.com>
2026-06-04 00:54:29 +0900

    update for auto enroll for loss function

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -2,13 +2,14 @@ import tensorflow as tf
 import numpy as np
 
 class Logger:
-    def __init__(self, split=None, keys=None):
+    def __init__(self, split=None):
         if split == "train":
-            keys = ["Epoch", "Ins/Img", "GPU", "CPU", "Cls_Loss", "Box_Loss", "Dfl_Loss", "LR"]
+            self.data = {"Epoch": "", "Ins/Img": "", "GPU": "", "CPU": "", "LR": ""}
         elif split == "val":
-            keys = ["Ins/Img", "GPU", "CPU", "mAP50", "mAP50:95", "Cls_Loss", "Box_Loss", "Dfl_Loss"]
-        self.keys = keys
-        self.data = {key: 0 for key in keys}
+            self.data = {"Ins/Img": "", "GPU": "", "CPU": "", "mAP50": "", "mAP50:95": ""}
+        else:
+            self.data = {}
+        self._fixed = list(self.data.keys())
 
     def update(self, **data):
         for key, value in data.items():
@@ -17,19 +18,19 @@ class Logger:
     def _to_scalar(self, value):
         if tf.is_tensor(value):
             value = value.numpy()
-        
         if isinstance(value, (np.ndarray, np.generic)):
             value = value.item()
-    
         return value
-    
+
+    @property
+    def keys(self):
+        return self._fixed + [k for k in self.data if k not in self._fixed]
+
     def __getitem__(self, key):
         return self.data[key]
-    
+
     def items(self):
         return self.data.items()
-    
+
     def values(self):
         return self.data.values()
-    
-    
